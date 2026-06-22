326cb20d2f5cbc70d876b224322b9faad2310a21
fikrudzulfahmi <fikrudzulfahmi@gmail.com>
2026-06-22 16:39:24 +0700

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Core Backend API - Almuhsin Universe</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <style>
         /* Mendaftarkan Color Palette dari Logo Almuhsin Universe */
