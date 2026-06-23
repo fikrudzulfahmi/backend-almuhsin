eb2906d970073f50e0a37419571d292344840b44
fikrudzulfahmi <fikrudzulfahmi@gmail.com>
2026-06-23 14:51:19 +0700

    revisi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Dedoc\Scramble\Scramble;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scramble::auth(function ($request) {
+            // return true artinya siapa saja boleh membuka halaman /docs ini
+            return true;
+        });
     }
 }
